b691298a07eab5c42947d295a2c3306978dfcde9
Connor McDowell <connormcdowell@Connors-Air-3.ht.home>
2024-11-21 16:57:00 -0500

    Added more components

---
+++ mycloset/app/components/Signin.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export const Signin = () => {
+    return(
+        <div>
+            <h1>Username</h1>
+            <h2>Password</h2>
+            <Link href="/">Log In</Link>
+        </div>
+    )
+}
--- mycloset/app/page.js
+++ mycloset/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Signin } from "./components/Signin"
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to myCloset</h1>
       <h2>Log In Place holder</h2>
+      <Signin/>
     </div>
   );
 }
