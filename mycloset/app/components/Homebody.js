a7e4866d0e5299dc78b2c0658532d135009bb84b
efl226 <efl226@lehigh.edu>
2024-11-30 20:06:14 -0500

    Home body has menu for sign in

---
+++ mycloset/app/components/Homebody.js
@@ -0,0 +1,28 @@
+
+
+export const Homebody = () => {
+    return(
+        <div style={{width: '100%', height: '100%', paddingLeft: 36, paddingRight: 36, paddingTop: 160, paddingBottom: 160, backgroundImage: 'url(https://via.placeholder.com/1440x890)', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex'}}>
+    <div style={{padding: 20, background: '#FAEDCD', borderRadius: 30, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: 10, display: 'flex'}}>
+        <div style={{width: 349, height: 153, position: 'relative'}}>
+            <div style={{width: 308, height: 85, left: 41, top: 68, position: 'absolute', color: 'black', fontSize: 64, fontFamily: 'Climate Crisis', fontWeight: '400', wordWrap: 'break-word'}}>Closet</div>
+            <div style={{width: 142, height: 88, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 64, fontFamily: 'Climate Crisis', fontWeight: '400', wordWrap: 'break-word'}}>my</div>
+        </div>
+        <div style={{height: 165, paddingLeft: 34, paddingRight: 34, paddingTop: 13, paddingBottom: 13, borderTopLeftRadius: 10, borderTopRightRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 25, display: 'flex'}}>
+            <div style={{width: 300, height: 57, paddingLeft: 18, paddingRight: 18, paddingTop: 9, paddingBottom: 9, background: '#CCD5AE', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30, justifyContent: 'flex-start', alignItems: 'center', gap: 153, display: 'inline-flex'}}>
+                <div style={{width: 68, color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Sign in</div>
+                <div style={{width: 35, height: 37, position: 'relative'}}>
+                    <div style={{width: 26.25, height: 30.84, left: 4.38, top: 3.07, position: 'absolute', background: 'black'}}></div>
+                </div>
+            </div>
+            <div style={{width: 300, height: 57, paddingLeft: 18, paddingRight: 18, paddingTop: 9, paddingBottom: 9, background: '#CCD5AE', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30, justifyContent: 'flex-start', alignItems: 'center', gap: 153, display: 'inline-flex'}}>
+                <div style={{width: 68, color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Upload</div>
+                <div style={{width: 35, height: 37, position: 'relative'}}>
+                    <div style={{width: 26.25, height: 30.84, left: 4.38, top: 3.07, position: 'absolute', background: 'black'}}></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    )
+}
