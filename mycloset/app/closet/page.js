f992e163b3def80f72b9bfd2b0787b761480df43
Connor McDowell <connormcdowell@dyn197188.cc.lehigh.edu>
2024-12-01 17:11:50 -0500

    Closet page edits

--- mycloset/app/closet/page.js
+++ mycloset/app/closet/page.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Client } from 'pg';
 
-export default async function Closet(){
+export default async function Closet({ username }){
+    const [closetItems, setClosetItems] = useState([])
+    const [userInfo, setUserInfo] = useState({})
+
+    //Fetches user closet on page render
+    useEffect(() => {
+        async function fetchData() {
+            try{
+                const res = await fetch(`/api/closet?username=${username}`)
+                const data = await res.json()
+                setClosetItems(data.closet)
+                setUserInfo(data.user)
+            } catch(err) {
+                console.error("Error fetching closet data")
+            }
+        }
+        fetchData()
+    }, [])
+
     return(
         <div>
             <h1>Closet</h1>
