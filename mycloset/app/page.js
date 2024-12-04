f21f3a2b7b0b166d22a7ead08338ebdc1efdac78
efl226 <efl226@lehigh.edu>
2024-12-03 21:38:00 -0500

    done done

--- mycloset/app/page.js
+++ mycloset/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Signin } from "./components/Signin"
 import { Homebody } from "./components/Homebody"
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <div>
       <Homebody/>
-      <Link href="/marketplace">Log In</Link>
+      <Link href="/marketplace" style = {{color: 'black', textDecoration: 'none'}}>Log In</Link>
     </div>
   );
 }
