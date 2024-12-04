e2dd0d3f9ea1cd2815b06a83bd8191c7e387accf
efl226 <efl226@lehigh.edu>
2024-12-03 20:08:18 -0500

    closet

--- mycloset/app/closet/page.js
+++ mycloset/app/closet/page.js
@@ -1,8 +1,10 @@
+import { Topbody } from "./Topbody";
 
 export default async function Closet(){
     return(
         <div>
             <h1>User Closet</h1>
+            <Topbody/>
         </div>
     )
 }
